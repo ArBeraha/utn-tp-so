e3b3278d1ac0ea5b2b9ec1d6ac23e03788f9a1a3
holi <sadasddsadsads@sadsadsadasdsa.com>
2016-04-20 14:47:58 -0300

    Empiezo con nucleo

--- src/nucleo/nucleo.c
+++ src/nucleo/nucleo.c
@@ -11,5 +11,21 @@
 
 int main()
 {
+	struct sockaddr_in direccionServidor;
+	direccionServidor = crearDireccionParaServidor(8080);
+	struct sockaddr_in direccionCliente;
+	unsigned int tamanioDireccion;
+	int servidor = socket_w();
+	int* i = 1;
+
+	bind_ws(servidor,&direccionServidor);
+
+	permitirReutilizacion(i);
+
+	listen_w(servidor);
+	printf("Estoy escuchando");
+
+	int cliente = accept(servidor, (void*)&direccionCliente,&tamanioDireccion);
+
 	return 0;
 }

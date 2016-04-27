39a7c74a6f3883bee0e300dcb0c30395d7f05de1
Lucas Esposito <espositolucas95@gmail.com>
2016-04-27 00:04:12 -0300

    Agrego un detalle al log, leer comentario del commit

--- src/otros/log.c
+++ src/otros/log.c
@@ -11,10 +11,16 @@
 #include <commons/log.h>
 #include "log.h"
 
+void iniciarLog()
+{
+	log_info(bgLogger,"\n\n***** Log de %s *****\n\n",bgLogger->program_name);
+}
+
 void crearLogs(char* logname, char* procName)
 {
 	activeLogger = log_create(string_from_format("%s.log",logname),procName,true,LOG_LEVEL_INFO);
 	bgLogger = log_create(string_from_format("%s.log",logname),procName,false,LOG_LEVEL_DEBUG);
+	iniciarLog();
 }
 
 void destruirLogs()

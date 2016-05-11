7e5ecda28d1994ca7f88a0aac19aebd99521d916
Lucas Esposito <espositolucas95@gmail.com>
2016-05-11 17:35:35 -0300

    Agrego proyecto de test para UMC

---
+++ testUMC/testCode/testUMC.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : testUMC.c
+ Author      : utnso
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "umc.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ umc/src/umc.h
@@ -0,0 +1,13 @@
+/*
+ * umc.h
+ *
+ *  Created on: 11/5/2016
+ *      Author: utnso
+ */
+
+#ifndef UMC_H_
+#define UMC_H_
+
+
+
+#endif /* UMC_H_ */

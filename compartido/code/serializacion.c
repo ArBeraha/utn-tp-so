9d1bef7f3ffbb3f47816d281b53dfd9ee45f863b
ensehn <ensehn@gmail.com>
2016-05-12 00:58:47 -0300

    Deserializacion STACK

--- compartido/code/serializacion.c
+++ compartido/code/serializacion.c
@@ -53,8 +53,8 @@ void deserializar_list(t_list* destino, char* fuente, int pesoElemento) {
 }
 
 int bytes_stack_item(t_stack_item* fuente) {
-	return sizeof(int) + list_size(fuente->argumentos) * sizeof(t_variable)
-			+ list_size(fuente->identificadores) * sizeof(t_identificador)
+	return sizeof(int) + list_size(fuente->argumentos) * sizeof(t_variable) +1
+			+ list_size(fuente->identificadores) * sizeof(t_identificador) +1
 			+ sizeof(int) + sizeof(t_variable);
 }
 
@@ -68,7 +68,7 @@ int bytes_stack(t_stack* fuente) {
 	return bytes;
 }
 
-void serializar_stack_item(char* destino, t_stack_item* fuente) {
+int serializar_stack_item(char* destino, t_stack_item* fuente) {
 	int offset = 0;
 	serializar_int(destino, fuente->posicion);
 	offset += sizeof(int);
@@ -78,23 +78,52 @@ void serializar_stack_item(char* destino, t_stack_item* fuente) {
 	offset += bytes;
 	destino[offset++] = list_size(fuente->identificadores);
 	bytes = list_size(fuente->identificadores) * sizeof(t_identificador);
-	serializar_list(destino + offset, fuente->argumentos,
+	serializar_list(destino + offset, fuente->identificadores,
 			sizeof(t_identificador));
 	offset += bytes;
 	serializar_int(destino, fuente->posicionRetorno);
 	offset += sizeof(int);
 	serializar_variable(destino, &(fuente->valorRetorno));
 	offset += sizeof(t_variable);
+	return offset; // Retorna el offset
 }
 
 void serializar_stack(char* destino, t_stack* fuente) {
 	int i, offset = 1;
 	t_stack_item* item;
-	destino[0] = stack_size(fuente); // Se va a guardar en la primera posicion la cantidad de elementos
+	destino[0] = stack_size(fuente); // Cantidad de items
 	for (i = 0; i < destino[0]; i++) {
 		item = list_get(fuente, i);
-		serializar_stack_item(destino + offset, item);
-		offset += bytes_stack_item(item);
+		offset+=serializar_stack_item(destino + offset, item); // Serial del item
 	}
 }
+
+int deserializar_stack_item(t_stack_item* destino, char* fuente){
+	int offset=0;
+	deserializar_int(destino->posicion,fuente[offset]);
+	offset+=sizeof(int);
+	destino->argumentos = list_create();
+	deserializar_list(destino->argumentos,fuente[offset],sizeof(t_variable));
+	offset+=sizeof(t_variable)*list_size(destino->argumentos)+1; // +1 por la cantidad de elementos de la lista
+	destino->identificadores = list_create();
+	deserializar_list(destino->identificadores,fuente[offset],sizeof(t_identificador));
+	offset+=sizeof(t_identificador)*list_size(destino->identificadores)+1; // +1 por la cantidad de elementos de la lista
+	deserializar_int(destino->posicionRetorno,fuente[offset]);
+	offset+=sizeof(int);
+	deserializar_variable(destino->valorRetorno,fuente[offset]);
+	offset+=sizeof(t_variable);
+	return offset;
+}
+
+void deserializar_stack(t_stack* destino, char* fuente){
+	t_stack_item* item;
+	int i,offset=1; // Cantidad de items en fuente[0]
+	for (i = 0; i < fuente[0]; i++) {
+		item = malloc(sizeof(t_stack_item)); // Tamaño del item (o sea de sus punteros tambien [NO DE LAS LISTAS EN SI])
+		offset+=deserializar_stack_item(item,fuente+offset);
+		stack_push(destino,item); // Agregar el item recibido al stack
+	}
+}
+
+
 
--- compartido/code/serializacion.h
+++ compartido/code/serializacion.h
@@ -21,6 +21,13 @@ void serializar_sentencia(char* destino,t_sentencia* fuente);
 void deserializar_sentencia(t_sentencia* destino, char* fuente);
 void serializar_list(char* destino,t_list* fuente, int pesoElemento);
 void deserializar_list(t_list* destino,char* fuente, int pesoElemento);
+int bytes_stack_item(t_stack_item* fuente);
+int bytes_stack(t_stack* fuente);
+int serializar_stack_item(char* destino, t_stack_item* fuente);
+void serializar_stack(char* destino, t_stack* fuente);
+int deserializar_stack_item(t_stack_item* destino, char* fuente);
+void deserializar_stack(t_stack* destino, char* fuente);
+
 
 #endif /* SERIALIZACION_H_ */
 

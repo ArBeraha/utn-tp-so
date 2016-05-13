218b72418fdefb0bcd9a71987ab49b07acbc9293
ensehn <ensehn@gmail.com>
2016-05-12 22:38:18 -0300

    Testeando Serialiacion

--- compartido/code/serializacion.c
+++ compartido/code/serializacion.c
@@ -48,25 +48,25 @@ int serializar_list(char* destino, t_list* fuente, int pesoElemento) {
 	}
 	return offset;
 }
-
 int deserializar_list(t_list* destino, char* fuente, int pesoElemento) {
-	int i, offset = 1;
-	int size = fuente[0]; // Se va a guardar en la primera posicion la cantidad de elementos
-	for (i = 0; i < size; i++) {
-		char* buffer = malloc(pesoElemento);
+	int i, offset = 1; // Se va a guardar en la primera posicion la cantidad de elementos
+	char* buffer;
+	for (i = 0; i < fuente[0]; i++) {
+		buffer = malloc(pesoElemento);
 		memcpy(buffer, fuente + offset, pesoElemento);
 		list_add(destino, buffer);
 		offset += pesoElemento;
 	}
 	return offset;
 }
-
+int bytes_list(t_list* fuente, int pesoElemento){
+	return list_size(fuente)*pesoElemento+1;
+}
 int bytes_stack_item(t_stack_item* fuente) {
 	return sizeof(int) + list_size(fuente->argumentos) * sizeof(t_variable) + 1
 			+ list_size(fuente->identificadores) * sizeof(t_identificador) + 1
 			+ sizeof(int) + sizeof(t_variable);
 }
-
 int bytes_stack(t_stack* fuente) {
 	int i, bytes = 0;
 	for (i = 0; i < stack_size(fuente); i++) {
@@ -74,7 +74,6 @@ int bytes_stack(t_stack* fuente) {
 	}
 	return bytes;
 }
-
 int serializar_stack_item(char* destino, t_stack_item* fuente) {
 	int offset = 0;
 	offset += serializar_int(destino, fuente->posicion);
@@ -84,22 +83,18 @@ int serializar_stack_item(char* destino, t_stack_item* fuente) {
 	offset += serializar_variable(destino, &(fuente->valorRetorno));
 	return offset; // Retorna el offset
 }
-
 int serializar_stack(char* destino, t_stack* fuente) {
 	int i, offset = 1;
-	t_stack_item* item;
 	destino[0] = stack_size(fuente); // Cantidad de items
 	for (i = 0; i < destino[0]; i++) {
-		item = list_get(fuente, i);
-		offset += serializar_stack_item(destino + offset, item); // Serial del item
+		offset += serializar_stack_item(destino + offset, list_get(fuente, i)); // Serial del item
 	}
 	return offset;
 }
-
 int deserializar_stack_item(t_stack_item* destino, char* fuente) {
 	int offset = 0;
-	destino->argumentos = list_create();
-	destino->identificadores = list_create();
+	destino->argumentos=list_create();
+	destino->identificadores=list_create();
 	offset += deserializar_int(destino->posicion, fuente + offset);
 	offset += deserializar_list(destino->argumentos, fuente + offset, sizeof(t_variable));
 	offset += deserializar_list(destino->identificadores, fuente + offset, sizeof(t_identificador));
@@ -107,7 +102,6 @@ int deserializar_stack_item(t_stack_item* destino, char* fuente) {
 	offset += deserializar_variable(&(destino->valorRetorno), fuente + offset);
 	return offset;
 }
-
 int deserializar_stack(t_stack* destino, char* fuente) {
 	t_stack_item* item;
 	int i, offset = 1; // Cantidad de items en fuente[0]
@@ -118,4 +112,61 @@ int deserializar_stack(t_stack* destino, char* fuente) {
 	}
 	return offset;
 }
+
+/* INICIO DE TESTS */
+void test_serializar_int(){
+	int a=57,b;
+	char* serial;
+	serial = malloc(sizeof(int));
+	serializar_int(serial,&a);
+	deserializar_int(&b,serial);
+	CU_ASSERT_EQUAL(a,b);
+}
+void test_serializar_variable(){
+	t_variable* varA=malloc(sizeof(t_variable));
+	t_variable*	varB=malloc(sizeof(t_variable));
+	char* serial=malloc(sizeof(t_variable));
+	serializar_variable(serial,varA);
+	deserializar_variable(varB,serial);
+	CU_ASSERT_EQUAL(varA->offset,varB->offset);
+	CU_ASSERT_EQUAL(varA->pagina,varB->pagina);
+	CU_ASSERT_EQUAL(varA->size,varB->size);
+}
+void test_serializar_sentencia(){
+	t_sentencia* senA=malloc(sizeof(t_sentencia));
+	t_sentencia* senB=malloc(sizeof(t_sentencia));
+	char* serial=malloc(sizeof(t_sentencia));
+	serializar_sentencia(serial,senA);
+	deserializar_sentencia(senB,serial);
+	CU_ASSERT_EQUAL(senA->offset_inicio,senB->offset_inicio);
+	CU_ASSERT_EQUAL(senA->offset_fin,senB->offset_fin);
+}
+void test_serializar_list(){
+	t_list *listA, *listB = NULL;
+	listA = list_create();
+	listB = list_create();
+	t_variable* var = malloc(sizeof(t_variable));
+	var->pagina=2;
+	list_add(listA,var);
+	char *serial = malloc(bytes_list(listA,sizeof(t_variable)));
+	serializar_list(serial,listA,sizeof(t_variable));
+	deserializar_list(listB,serial,sizeof(t_variable));
+	CU_ASSERT_EQUAL(((t_variable*)list_get(listB,0))->pagina,2);
+	list_destroy(listA);
+	list_destroy(listB);
+}
+
+int test_serializacion(){
+	CU_initialize_registry();
+	CU_pSuite suite_serializacion = CU_add_suite("Suite de prueba", NULL, NULL);
+	CU_add_test(suite_serializacion, "Test de Serializacion de int", test_serializar_int);
+	CU_add_test(suite_serializacion, "Test de Serializacion de t_variable", test_serializar_variable);
+	CU_add_test(suite_serializacion, "Test de Serializacion de t_sentencia", test_serializar_sentencia);
+	CU_add_test(suite_serializacion, "Test de Serializacion de t_list", test_serializar_list);
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	CU_cleanup_registry();
+	return CU_get_error();
+}
+
 
--- compartido/code/serializacion.h
+++ compartido/code/serializacion.h
@@ -9,8 +9,11 @@
 #define SERIALIZACION_H_
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "commonTypes.h"
 #include <commons/collections/list.h>
+#include "CUnit/Basic.h"
 
 void imprimir_serializacion(char* serial, int largo);
 int serializar_int(char* destino,int* fuente);
@@ -21,6 +24,7 @@ int serializar_sentencia(char* destino,t_sentencia* fuente);
 int deserializar_sentencia(t_sentencia* destino, char* fuente);
 int serializar_list(char* destino,t_list* fuente, int pesoElemento);
 int deserializar_list(t_list* destino,char* fuente, int pesoElemento);
+int bytes_list(t_list* fuente, int pesoElemento);
 int bytes_stack_item(t_stack_item* fuente);
 int bytes_stack(t_stack* fuente);
 int serializar_stack_item(char* destino, t_stack_item* fuente);
@@ -28,6 +32,12 @@ int serializar_stack(char* destino, t_stack* fuente);
 int deserializar_stack_item(t_stack_item* destino, char* fuente);
 int deserializar_stack(t_stack* destino, char* fuente);
 
+// Tests
+int test_serializacion();
+void test_serializar_int();
+void test_serializar_variable();
+void test_serializar_sentencia();
+void test_serializar_list();
 
 #endif /* SERIALIZACION_H_ */
 

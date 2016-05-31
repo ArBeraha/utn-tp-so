4f5751990c23bc60c48506b3619630b72f8093e2
Lucas Esposito <espositolucas95@gmail.com>
2016-05-31 15:09:39 -0300

    Agrego parametro a logs

--- compartido/code/log.c
+++ compartido/code/log.c
@@ -16,10 +16,27 @@ void iniciarLog()
 	log_info(bgLogger,"\n\n***** Log de %s *****\n\n",bgLogger->program_name);
 }
 
-void crearLogs(char* logname, char* procName)
+/**
+ * logLevel:
+ * LOG_PRINT_NOTHING
+ * LOG_PRINT_DEFAULT
+ * LOG_PRINT_ALL
+ */
+void crearLogs(char* logname, char* procName, int logLevel)
 {
-	activeLogger = log_create(string_from_format("%s.log",logname),procName,true,LOG_LEVEL_INFO);
-	bgLogger = log_create(string_from_format("%s.log",logname),procName,false,LOG_LEVEL_DEBUG);
+	bool activeLoggerIsActive = true;
+	bool bgLoggerIsActive = false;
+	if(logLevel>=3 || logLevel <0){
+		perror("LogLevel >=3 || logLevel <0.");
+	}else{
+		switch(logLevel){
+		case 0: activeLoggerIsActive=false;break;
+		case 1: break; // default.
+		case 2: bgLoggerIsActive=true; break;
+		}
+	}
+	activeLogger = log_create(string_from_format("%s.log",logname),procName,activeLoggerIsActive,LOG_LEVEL_INFO);
+	bgLogger = log_create(string_from_format("%s.log",logname),procName,bgLoggerIsActive,LOG_LEVEL_DEBUG);
 	iniciarLog();
 }
 
--- compartido/code/log.h
+++ compartido/code/log.h
@@ -7,11 +7,18 @@
 
 #ifndef OTROS_LOG_H_
 #define OTROS_LOG_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LOG_PRINT_NOTHING 0
+#define LOG_PRINT_DEFAULT 1
+#define LOG_PRINT_ALL 2
+
 
 t_log *activeLogger, *bgLogger;
 
 
-void crearLogs(char* logname, char* procName);
+void crearLogs(char* logname, char* procName, int logLevel);
 void destruirLogs();
 
 #endif /* OTROS_LOG_H_ */

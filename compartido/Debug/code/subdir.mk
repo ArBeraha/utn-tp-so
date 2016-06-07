89f101c445b83c6c44fb7203993e29d23028ce37
Lucas Esposito <espositolucas95@gmail.com>
2016-06-07 00:04:32 -0300

    Ultimo fix de compilacion

---
+++ compartido/Debug/code/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../code/cliente-servidor.c \
+../code/commonTypes.c \
+../code/header.c \
+../code/log.c \
+../code/serializacion.c \
+../code/stack.c 
+
+OBJS += \
+./code/cliente-servidor.o \
+./code/commonTypes.o \
+./code/header.o \
+./code/log.o \
+./code/serializacion.o \
+./code/stack.o 
+
+C_DEPS += \
+./code/cliente-servidor.d \
+./code/commonTypes.d \
+./code/header.d \
+./code/log.d \
+./code/serializacion.d \
+./code/stack.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+code/%.o: ../code/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

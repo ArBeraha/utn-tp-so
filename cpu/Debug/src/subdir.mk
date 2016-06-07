c08bb7ef94481d40e812e850d8da2e8ca90e6dd6
Lucas Esposito <espositolucas95@gmail.com>
2016-06-06 22:33:19 -0300

    Update make de cpu

--- cpu/Debug/src/subdir.mk
+++ cpu/Debug/src/subdir.mk
@@ -4,16 +4,19 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/auxiliaresDePrimitivas.c \
 ../src/cpu.c \
 ../src/primitivas.c \
 ../src/test.c 
 
 OBJS += \
+./src/auxiliaresDePrimitivas.o \
 ./src/cpu.o \
 ./src/primitivas.o \
 ./src/test.o 
 
 C_DEPS += \
+./src/auxiliaresDePrimitivas.d \
 ./src/cpu.d \
 ./src/primitivas.d \
 ./src/test.d 

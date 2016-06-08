f3ccde36217119a81ea55c88050d35cc01b8d690
ensehn <ensehn@gmail.com>
2016-06-08 18:46:35 -0300

    [INVENTO] Macros para mutex

--- compartido/code/hilos.h
+++ compartido/code/hilos.h
@@ -15,6 +15,12 @@
 pthread_attr_t detachedAttr;
 t_list* mutexs;
 
+// Dejo un macro para no tener que estar rodeando todo con mutexs, pueden creer otro macro en base a este por cada mutex especifico
+#define MUTEX(CONTENIDO,MUTX) \
+	{pthread_mutex_lock(&MUTX); \
+	CONTENIDO; \
+	pthread_mutex_unlock(&MUTX);} \
+
 void iniciarAtrrYMutexs(int cantidadDeMutexs, ...);
 int crearHilo(pthread_t * hilo,   void *(*funcion) (void *));
 int crearHiloConParametro(pthread_t * hilo,   void *(*funcion) (void *), void * parametro);

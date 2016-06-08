892e19560b88c8fd678843be24d212f93027741f
ensehn <ensehn@gmail.com>
2016-06-08 00:07:44 -0300

    iniciarAtrrYMutexs()/finalizarAtrrYMutexs() todo en un llamado

--- compartido/code/hilos.c
+++ compartido/code/hilos.c
@@ -7,12 +7,28 @@
 
 #include "hilos.h"
 
-void configHilos() {
+void iniciarAtrrYMutexs(int cantidadDeMutex, ...) {
 	pthread_attr_init(&detachedAttr);
 	pthread_attr_setdetachstate(&detachedAttr, PTHREAD_CREATE_DETACHED);
+	mutexs = list_create();
+	va_list arguments;
+	va_start(arguments, cantidadDeMutex);
+	int i;
+	for (i = 0; i < cantidadDeMutex; i++) {
+		void* mutex = va_arg(arguments, void*);
+		pthread_mutex_init(mutex, NULL);
+		list_add(mutexs, mutex);
+	}
+	va_end(arguments);
 }
-
-int crearHilo(pthread_t * hilo,   void *(*funcion) (void *)){
-	return pthread_create(&hilo, &detachedAttr, (void*) funcion, NULL);
+int crearHilo(pthread_t * hilo, void *(*funcion)(void *)) {
+	return pthread_create(hilo, &detachedAttr, (void*) funcion, NULL);
 }
-
+int crearHiloConParametro(pthread_t * hilo, void *(*funcion)(void *),
+		void * parametro) {
+	return pthread_create(hilo, &detachedAttr, (void*) funcion, parametro);
+}
+void finalizarAtrrYMutexs() {
+	list_destroy_and_destroy_elements(mutexs, (void*) pthread_mutex_destroy);
+	pthread_attr_destroy(&detachedAttr);
+}
--- compartido/code/hilos.h
+++ compartido/code/hilos.h
@@ -9,7 +9,15 @@
 #define CODE_HILOS_H_
 
 #include <pthread.h>
+#include <stdarg.h>
+#include <commons/collections/list.h>
 
 pthread_attr_t detachedAttr;
+t_list* mutexs;
+
+void iniciarAtrrYMutexs(int cantidadDeMutexs, ...);
+int crearHilo(pthread_t * hilo,   void *(*funcion) (void *));
+int crearHiloConParametro(pthread_t * hilo,   void *(*funcion) (void *), void * parametro);
+void finalizarAtrrYMutexs();
 
 #endif /* CODE_HILOS_H_ */
